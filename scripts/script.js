07fb704a84d60427177af78f9200c68389a22f82
brandoncoplen <89221350+brandoncoplen@users.noreply.github.com>
2021-09-19 20:37:56 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,68 @@
 $(function() {
     console.log("document is ready!");
 
-    $("#doge-meme-pic").draggable({
-        containment: "#containment-wrapper",
-        scroll: false,
-        stop: function() {
-            calculateWow();
+    var userName, nameLength, nameMsg, nameReverse = ""; 
+
+    $('#btnName').click(function() {
+        userName = $('#userName').val(); 
+        // console.log(userName); 
+        // console.log($('#userName').val().length); 
+        // console.log(getStringLength(userName)); 
+
+        nameLength = getStringLength(userName);
+        nameMsg = "The Length of your name is: ";
+        $('#nameOutput').append(nameMsg + nameLength + "<br/>"); 
+
+        // console.log(reverseString(userName)); 
+
+        nameReverse = reverseString(userName);
+        nameMsg = "Your Name reversed letters is: ";
+        $('#nameOutput').append(nameMsg + nameReverse + "<br/>"); 
+
+        console.log(thirdLetter(userName)); 
+
+        stringArrayPosition(userName); 
+
+        });
+
+        $('#btnClear').click(function(){
+            clearElement("#nameOutput", "html");
+            clearElement("#userName", "input");
+      })
+
+    function getStringLength(stringVal){
+        var stringLength = stringVal.length; 
+       return stringLength; 
+    }
+
+    function reverseString(stringVal){
+        var revString = stringVal.split("").reverse().join("");
+       return revString; 
+    }
+
+    function thirdLetter(stringVal){
+        
+       return thirdString; var thirdString = stringVal[2]; 
+    }
+
+    function clearElement(elementId, elementType){
+        if(elementType == "html"){
+            $(elementId).html(""); 
         }
-    });
-
-    function calculateWow() {
-        var x = $("#doge-meme-pic").position();
-        var wow = x.top + x.left;
-
-        if (wow < 500) {
-
-            console.log('not much wow (' + wow + ')');
-            $('#wow-output').html('<h2>not much wow (' + wow + ')</h2>')
-
-        } else {
-            console.log('so much wow (' + wow + ')!!!!');
-            $('#wow-output').text('so much wow (' + wow + ')!!')
+        else if(elementType=="input"){
+            $(elementId).val(" "); 
         }
+      
+        
+    } 
+
+    function stringArrayPosition(stringVal){
+
+        for(i=0; i<stringVal.length; i++){
+            console.log(i + " " + stringVal[i]);
+        }
+
+
     }
 
 });
